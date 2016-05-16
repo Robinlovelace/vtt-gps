8a790d39e9df8512e2196b8ec913cc1be58da601
Robin <rob00x@gmail.com>
2016-05-16 16:09:46 +0100

    Update road types code

--- R/road-types.R
+++ R/road-types.R
@@ -6,8 +6,29 @@ source("R/load-data.R")
 
 b = bb(i1)
 
-h = osmplotr::extract_osm_objects(key = 'highway', bbox = b)
+# create all highways
+h = extract_osm_objects(key = 'highway', bbox = b)
+
+# save 1 road type ???
+summary(h)
+h$
+
 plot(h)
 plot(i1, add = T, col = "red")
 
-bi1 = 
+bi1 = stplanr::buff_geo(i1, width = 50)
+
+# see if bi1 (buffer of i1) look right
+plot(bi1, add = T, col = "green")
+
+# chop out all road network in the buff
+ri1 = gIntersection(bi1, h)
+
+# add information back to track data - needs in metres
+itea$main_road[1] = gLength(ri1)
+
+plot(ri1, add = T)
+plot(ri1)
+class(ri1)
+summary(ri1)
+
